6460b3aef9e62a36f3871bf33879fb34418e3434
Karan-Developer-69 <homepc8890@gmail.com>
2025-04-20 16:02:49 +0530

    Update PdfRouter.js

--- Server/routes/PdfRouter.js
+++ Server/routes/PdfRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/get-pdfs",(req,res)=>{
+    console.log("debug: Request recived ")
     res.status(200).json([
         { id: 1, image: 'https://picsum.photos/id/1011/300/200', title: 'React in Depth PDF', price: '499', rating: 0, reviews: 0,  downloads: 0 },
         { id: 2, image: 'https://picsum.photos/id/1012/300/200', title: 'Advanced JavaScript Guide', price: '599', rating: 0, reviews: 0,  downloads: 0 },
